567ecfd317e73c575b2cafbc4f2f81235c82efe3
PunNapat240 <punpun.naput@gmail.com>
2025-03-28 04:13:31 +0700

    u

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-apt-get update && apt-get install -y tesseract-ocr
+apt-get update && apt-get install -y tesseract-ocr tesseract-ocr-tha
+pip install -r requirements.txt
 gunicorn server:app
