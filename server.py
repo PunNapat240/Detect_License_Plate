f3a73f09c4165b9f2f991333c657199f33cf46ab
PunNapat240 <punpun.naput@gmail.com>
2025-03-28 04:03:49 +0700

    up

--- server.py
+++ server.py
@@ -10,7 +10,8 @@ UPLOAD_FOLDER = "static/uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # กำหนด path ของ Tesseract OCR (แก้ไขให้ตรงกับเครื่องของคุณ)
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 
 def apply_gamma_correction(image, gamma=1.5):
     """ ปรับค่า Gamma Correction เพื่อเพิ่มความคมชัดของภาพ """
